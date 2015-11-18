b0aa863fd27ab6c64ba1e2eff70cdc930f8b424c
QUINOESCOBAR <quinoescobar@gmail.com>
2015-11-18 11:48:26 +0000

    creado answer_model.js

---
+++ models/answer_model.js
@@ -0,0 +1,13 @@
+
+function Answer(args) {
+  // body...
+  // this.funci = funci;
+  // this.cadena = cadena;
+  // this.exreg = exreg;
+  this.args = args;
+  this.run = function() {
+	    alert("evaluando "+this.args);
+
+      //aqui codigo de clasificacion de que tipo de argumento se ingresó
+	  };
+}
