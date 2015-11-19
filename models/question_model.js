6a9774ebea0c2dcd3dcc3b7ca69baf0f019df077
QUINOESCOBAR <quinoescobar@gmail.com>
2015-11-19 20:47:05 +0000

    question_model, creadas la clase madre e hijos

--- models/question_model.js
+++ models/question_model.js
@@ -2,12 +2,19 @@
 
 function Question(argument) {
   // body...
+  this.argument=argument;
 }
 
 function ShortQuestion(argument) {
   // body...
+  Question.call(this.argument);
 }
 
 function LongQuestion(argument) {
   // body...
+  Question.call(this.argument);
 }
+
+module.exports = Question;
+module.exports = Question;
+module.exports = Question;
